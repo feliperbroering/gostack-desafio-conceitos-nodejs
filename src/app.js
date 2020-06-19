92833fbd2433924f61d9a0bf9a4e29f127887353
Felipe R. Broering <felipe.broering@gmail.com>
2020-06-19 01:34:43 -0300

    funcionamento das rotas implementadas

--- src/app.js
+++ src/app.js
@@ -1,33 +1,82 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
+let repositories = [];
+
+findRepository = (request) => {
+  const { id } = request.params;
+  const index = repositories.findIndex(repository => repository.id === id);
+  let repository = null;
+  if ( index > -1)
+    repository = repositories[index];
+  return { index, repository };
+}
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  
+  let { index, repository } = findRepository(request);
+
+  if (!repository)
+    return response.sendStatus(400);
+    
+  repository.title = title;
+  repository.url = url;
+  repository.techs = techs;
+
+  repositories[index] = repository;
+  
+  return response.json(repositories[index]);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  let { index, repository } = findRepository(request);
+
+  if (!repository)
+    return response.sendStatus(400);
+  
+  repositories.splice(index, 1);
+
+  return response.sendStatus(204);
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  let { index, repository } = findRepository(request);
+
+  if (!repository)
+    return response.sendStatus(400);
+
+  repositories[index].likes++;
+
+  return response.json(repositories[index]);
 });
 
 module.exports = app;
